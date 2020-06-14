c760dd779aafa321aa2c58147941a524772497bc
Harry Man <harryman@Harrys-MacBook-Air-3.local>
2020-06-14 11:43:53 +0100

    Removing lines from footer adding website and links

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 ((d) => {
 
     let clickCount = 0;
+    let textCount = 0;
     let billboardBackground = d.getElementById("billboard");
     let billboardText = d.getElementById("slogan");
 
@@ -9,26 +10,25 @@
     let saturation = 50;
     let lightness = 50;
 
+    const testJSON = ['Pay Someone a Compliment', 
+    'Draw a Picture for Someone You Love', 
+    'Don\'t be ashamed to use your own ideas',
+    'Treat yourself once in a while', 
+    'Look at the order not the structure']
 
     billboardBackground.addEventListener("click", () => {
-        clickCount +=24;
-
+        clickCount +=50;
         hue = Math.round(clickCount);
-
         let hsl = "hsl(" + hue + ", " + saturation + "%, " + lightness + "%)"; 
         console.log(hsl);
-
         body.style.backgroundColor = `${hsl}`;
     });
 
     billboardText.addEventListener("click", () => {
-    )
+        let randomNumber = Math.floor(Math.random()*11);
+        textCount = (textCount + randomNumber) % testJSON.length;
+        let newText = testJSON[textCount];
+        billboardText.innerHTML = `${newText}`
+    });
 
 })(document);
-
-
-// d.getElementById("slogan").onclick = changeText;
-
-// function changeText() {
-//     d.getElementById("slogan").innerHTML = "Dodge Bullets";
-//     return false;  
